54b23de4d57c203aa4121aa9137a8e404d197a57
Elliott Weix <elliott@weix.us>
2026-08-02 11:05:00 -0500

    fix: allow undeclared names to pass

--- auto-print/turf-pdf-to-csv_v2.py
+++ auto-print/turf-pdf-to-csv_v2.py
@@ -88,7 +88,10 @@ def extract_data(
 def post_process(data):
     """Canonize names and apply post-processing for duplicate detection"""
     canonical = [
-        get_close_matches(r[1], DISTRICT_NAMES, n=1, cutoff=0.6)[0] for r in data[1:]
+        get_close_matches(r[1], DISTRICT_NAMES, n=1, cutoff=0.6)[0]
+        if get_close_matches(r[1], DISTRICT_NAMES, n=1, cutoff=0.6)[0]
+        else r[1]
+        for r in data[1:]
     ]
     ward_turf = [f"{canonical[i]} - {r[2]} - {r[3]}" for i, r in enumerate(data[1:])]
     dup_finder = [f"{ward_turf[i]} - {r[5]}" for i, r in enumerate(data[1:])]
